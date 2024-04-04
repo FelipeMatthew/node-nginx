dbce47fae03892b18421a75f639adc5228d3ca50
felipe <felipematthewnascimento@gmai.com>
2024-04-03 22:12:12 -0300

    :bug: Fix: Fixing nginx & Db dependence

--- node/index.js
+++ node/index.js
@@ -19,7 +19,7 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Running on port 8080');
+  console.log('Running on port 3000');
 
   const createSql = `
     CREATE TABLE IF NOT EXISTS people (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY (id));
